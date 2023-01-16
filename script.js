0584a1570c799eab587740998ad81470a98f2d81
YamaHaya/山隼 <114995011+Yama-Haya@users.noreply.github.com>
2023-01-16 13:30:33 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,71 @@ qa[6] = ["トンボを英語にするとどれ？","fly","dragonfly","butterfly"
 qa[7] = ["ヒトデを英語にするとどれ？","starfish","starshell","starmine",1];
 qa[8] = ["恒星の中で最も明るい星は？","デネブ","スピカ","シリウス",3];
 qa[9] = ["惑星の中で最も重たいのはどれ？","太陽","木星","天王星",2];
+
+//初期設定
+q_sel = 3; //選択肢の数
+setReady();
+
+//初期設定
+function setReady() {
+	count = 0; //問題番号
+	ansers = new Array(); //解答記録
+	
+	//最初の問題
+	quiz();
+}
+
+//問題表示
+function quiz() {
+	var s, n;
+	//問題
+	document.getElementById("text_q").innerHTML = (count + 1) + "問目：" + qa[count][0];
+	//選択肢
+	s = "";
+	for (n=1;n<=q_sel;n++) {
+		s += "【<a href=''>" + n + "：" + qa[count][n] + "</a>】";
+	}
+	document.getElementById("text_s").innerHTML = s;
+}
+//解答表示
+function anser(num) {
+	var s;
+	s = (count + 1) + "問目：";
+	//答え合わせ
+	if (num == qa[count][q_sel + 1]) {
+		//正解
+		s += "○" + qa[count][num];
+    ansers[count] = "○";
+	} else {
+		s += "×" + qa[count][num];
+    ansers[count] = "×";
+	}
+	document.getElementById("text_a").innerHTML = s;
+  //次の問題を表示
+	count++;
+	if (count < qa.length) {
+		quiz();
+	} else {
+		//終了
+    s = "<table border='2'><caption>成績発表</caption>";
+		//1行目
+		s += "<tr><th>問題</th>";
+		for (n=0;n<qa.length;n++) {
+			s += "<th>" + (n+1) + "</th>";
+		}
+		s += "</tr>";
+		//2行目
+		s += "<tr><th>成績</th>";
+		for (n=0;n<qa.length;n++) {
+			s += "<td>" + ansers[n] + "</td>";
+		}
+		s += "</tr>";
+		s += "</table>";
+		document.getElementById("text_q").innerHTML = "";
+    //次の選択肢
+		s = "【<a href='javascript:history.back()'>前のページに戻る</a>】";
+		s += "【<a href='javascript:setReady()'>同じ問題を最初から</a>】";
+		s += "【<a href=''>次の問題に進む</a>】";
+		document.getElementById("text_s").innerHTML = "";
+	}
+}
