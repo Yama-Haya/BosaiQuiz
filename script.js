48aea266c56b1206e0ca84b44cc846ed23d1d465
YamaHaya/山隼 <114995011+Yama-Haya@users.noreply.github.com>
2023-01-16 13:23:12 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-var lang = navigator.language;
-if (lang==ja||lang==ja_JP||lang==js-JP||lang==null){
-  $.getJSON('https://yama-haya.github.io/BosaiQuiz/ja.json')
-}
-if (lang==en||lang==en_US||lang==en-US){
-  $.getJSON('https://yama-haya.github.io/BosaiQuiz/en.json')
-}
+
+qa = new Array();
+qa[0] = ["イルカを漢字で書くとどれ？","海豚","海牛","河豚",1];
+qa[1] = ["クラゲを漢字で書くとどれ？","水浮","水母","水星",2];
+qa[2] = ["カタツムリを漢字で書くとどれ？","禍牛","鍋牛","蝸牛",3];
+qa[3] = ["バッタを漢字で書くとどれ？","飛蝗","飛蟻","飛脚",1];
+qa[4] = ["タツノオトシゴを英語にするとどれ？","sea fish","sea horse","sea dragon",2];
+qa[5] = ["マグロを英語にするとどれ？","funa","suna","tuna",3];
+qa[6] = ["トンボを英語にするとどれ？","fly","dragonfly","butterfly",2];
+qa[7] = ["ヒトデを英語にするとどれ？","starfish","starshell","starmine",1];
+qa[8] = ["恒星の中で最も明るい星は？","デネブ","スピカ","シリウス",3];
+qa[9] = ["惑星の中で最も重たいのはどれ？","太陽","木星","天王星",2];
