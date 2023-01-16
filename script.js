3feafd8f0538cc8ab2fc0c3467bc08f6228aaec2
YamaHaya/山隼 <114995011+Yama-Haya@users.noreply.github.com>
2023-01-16 11:06:24 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+var lang = navigator.language;
+if (lang==ja||lang==ja_JP||lang==js-JP||lang==null){
+  $.getJSON('https://yama-haya.github.io/BosaiQuiz/ja.json')
+}
+if (lang==en||lang==en_US||lang==en-US){
+  $.getJSON('https://yama-haya.github.io/BosaiQuiz/en.json')
+}
