e45c8e3b980c58fd8e457eafb771f3e818e07103
YamaHaya/山隼 <114995011+Yama-Haya@users.noreply.github.com>
2023-01-30 19:36:23 +0900

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,52 @@
+// アプリケーション作成用のモジュールを読み込み
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+// メインウィンドウ
+let mainWindow;
+
+const createWindow = () => {
+  // メインウィンドウを作成します
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // プリロードスクリプトは、レンダラープロセスが読み込まれる前に実行され、
+      // レンダラーのグローバル（window や document など）と Node.js 環境の両方にアクセスできます。
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // メインウィンドウに表示するURLを指定します
+  // （今回はmain.jsと同じディレクトリのindex.html）
+  mainWindow.loadFile("index.html");
+
+  // デベロッパーツールの起動
+  mainWindow.webContents.openDevTools();
+
+  // メインウィンドウが閉じられたときの処理
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+};
+
+//  初期化が完了した時の処理
+app.whenReady().then(() => {
+  createWindow();
+
+  // アプリケーションがアクティブになった時の処理(Macだと、Dockがクリックされた時）
+  app.on("activate", () => {
+    // メインウィンドウが消えている場合は再度メインウィンドウを作成する
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// 全てのウィンドウが閉じたときの処理
+app.on("window-all-closed", () => {
+  // macOSのとき以外はアプリケーションを終了させます
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
